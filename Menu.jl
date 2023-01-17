1b17be5f2231051580975d6f8b627215c378f379
Diegofs3 <dfarias2017@udec.cl>
2023-01-16 21:31:34 -0300

    menu v1

--- Menu.jl
+++ Menu.jl
@@ -2,6 +2,11 @@
 print("Ingrese su nombre: ")
 x = readline()
 print("hola $x")
-print("Que haras hoy $x")
+print("Que abro hoy $x")
 y = readline()
 print(y)
+if y == "chiste"
+    print("Tengo una enfermedad que me obliga a hacer chiste de aeropuertos")
+    wait(5)
+    print("El doctor dice que es terminal")
+end
